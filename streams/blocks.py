65722d63d22ed0d0927890f08f71ab80d0f15107
educa77 <eduardo.calo@egoargentina.com>
2021-07-14 15:24:47 -0300

    agregando botones con link internal o external en bloque

--- streams/blocks.py
+++ streams/blocks.py
@@ -74,3 +74,32 @@ class CTABlock(blocks.StructBlock):
         template = "streams/cta_block.html"
         icon = 'placeholder'
         label = 'Call to Action'
+
+
+class LinkStructValue(blocks.StructValue):
+    """Additional logic for our urls"""
+
+    def url(self):
+        button_page = self.get('button_page')
+        button_url = self.get('button_url')
+        if button_page:
+            return button_page.url
+        elif button_url:
+            return button_url
+
+        return None
+
+
+class ButtonBlock(blocks.StructBlock):
+    """An internal or external URL"""
+
+    button_page = blocks.PageChooserBlock(
+        required=False, help_text='If selected, this url will be used first')
+    button_url = blocks.URLBlock(
+        required=False, help_text='If added, this url will be used secondarily')
+
+    class Meta:
+        template = "streams/button_block.html"
+        icon = 'placeholder'
+        label = 'Single Button'
+        value_class = LinkStructValue
